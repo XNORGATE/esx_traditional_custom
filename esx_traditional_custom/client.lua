aa626cc4ae101f75be685472fda980c0ed524a5e
FlizzyAC <52878239+FlizzyAC@users.noreply.github.com>
2021-02-12 11:36:04 +0800

    performance improvement

--- esx_traditional_custom/client.lua
+++ esx_traditional_custom/client.lua
@@ -1,10 +1,6 @@
-ESX                           = nil
 Citizen.CreateThread(function ()
     while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) 
-            ESX = obj 
-        end)
-
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
         Citizen.Wait(5)
     end
 end)
@@ -12,38 +8,34 @@ end)
 local templePos = vec(492.28, -1039.66, 28.34)
 
 RegisterNetEvent('xng:usedToss')
-AddEventHandler('xng:usedToss', function(name)
+AddEventHandler('xng:usedToss', function()
 	local ped = PlayerPedId()
     if #(GetEntityCoords(ped) - templePos) <= 2 then
 		ESX.ShowNotification(wish_custom)
 		Citizen.Wait(3000)
 
-        TriggerEvent('esx:showNotification', doing_custom)
+		ESX.ShowNotification(doing_custom)
 		xng_showAnim()	
-		Citizen.Wait(5000)
+		Citizen.Wait(5000 + 6500)
 
-        local rate = math.random(1,3)
-		
+        local rate = math.random(1,3)		
         if rate == 1 then 
-			Citizen.Wait(6500)
 			PlaySound(-1, 'Menu_Accept', 'Phone_SoundSet_Default', 0, 0, 1)
-			ESX.Scaleform.ShowFreemodeMessage(TossResult_nice, name..TossResult, 5)
+			ESX.Scaleform.ShowFreemodeMessage(TossResult_nice, GetPlayerName(PlayerId())..TossResult, 5)
 			ESX.ShowNotification(TossResult_nice_mean)
             TriggerEvent('esx:showNotification', got_money)    
-            TriggerServerEvent('xng:tossResult', 'money')
+            TriggerServerEvent('xng:tossedMoney')
         elseif rate == 2 then 
-			Citizen.Wait(6500)
 			PlaySound(-1, 'Menu_Accept', 'Phone_SoundSet_Default', 0, 0, 1)
-			ESX.Scaleform.ShowFreemodeMessage(TossResult_hmmm, name..TossResult, 5)
+			ESX.Scaleform.ShowFreemodeMessage(TossResult_hmmm, GetPlayerName(PlayerId())..TossResult, 5)
 			ESX.ShowNotification(TossResult_hmmm_mean)   
         elseif rate == 3 then
-			Citizen.Wait(6500)
 			PlaySound(-1, 'Menu_Accept', 'Phone_SoundSet_Default', 0, 0, 1)
-			ESX.Scaleform.ShowFreemodeMessage(TossResult_nah, name..TossResult, 5)
+			ESX.Scaleform.ShowFreemodeMessage(TossResult_nah, GetPlayerName(PlayerId())..TossResult, 5)
 			ESX.ShowNotification(TossResult_nah_mean)
         end
     else
-        TriggerEvent('esx:showNotification', wrong_place)    
+		ESX.ShowNotification(wrong_place)
     end   
 end)
 
@@ -55,8 +47,7 @@ function xng_showAnim()
 	Citizen.Wait(5000)
 	ClearPedTasks(PlayerPedId())
 end
-  
-  
+
 function loadAnimDict(dict)
 	while (not HasAnimDictLoaded(dict)) do
 		RequestAnimDict(dict)
